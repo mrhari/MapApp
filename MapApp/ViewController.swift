0a94a2453dd48407e2ca368cdd7fb795779d78fb
Ngo Van Hai <hainv@ngos-macbook-pro.local>
2018-09-18 17:32:54 +0700

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Ngo Van Hai on 9/18/18.
+//  Copyright © 2018 Ngo Van Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
